a4c1f420bee004185c101da295aa3f26c18f4928
Andrea Mourino <amourinodesousa@danielcastelao.org>
2025-06-11 13:35:30 +0200

    Segundo Test

--- src/com/cod/mvc/ControllerTest.java
+++ src/com/cod/mvc/ControllerTest.java
@@ -5,7 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class ControllerTest {
     @org.junit.jupiter.api.Test
     void crearCoche() {
-    } //close issue
+    } //close issue #2
 
 
+    @org.junit.jupiter.api.Test
+    void cambiarVelocidad() {
+    }
 }
